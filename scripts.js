2320a678ca25bd79dea573346fd1342d0631e429
Mariya Kapitanchyk <mari.kapitanchik@mail.ru>
2021-05-19 15:52:31 +0200

    ggg

---
+++ scripts.js
@@ -0,0 +1,32 @@
+
+
+
+var placeT = document.createElement("h2");
+var task = document.createTextNode("Tasks");
+placeT.appendChild(task);
+var element = document.getElementById("div1");
+element.appendChild(placeT);
+
+
+
+
+// <div id="div1" class="header">
+//         <h2 style="margin:5px">Tasks</h2>
+//         <input type="text" id="myInput" placeholder="Название...">
+//         <span onclick="newElement()" class="addBtn">Добавить</span>
+//       </div>
+
+
+
+
+
+//       <div id="myDIV" class="header">
+//     <h2 style="margin:5px">Tasks</h2>
+//     <input type="text" id="myInput" placeholder="Название...">
+//     <span onclick="newElement()" class="addBtn">Добавить</span>
+//   </div>
+
+
+
+
+var a = document.getElementsByTagName('ul');
